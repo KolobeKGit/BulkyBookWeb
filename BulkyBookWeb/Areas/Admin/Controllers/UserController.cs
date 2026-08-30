f31fd54e5c4f6c94129bb847307b010bd908e419
KolobeKGit <kamo.kolobe@gmail.com>
2026-08-30 13:52:46 +0200

    User Management with Lock, Unlock and Permission added

---
+++ BulkyBookWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,241 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        // GET: CompanyController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult Upsert(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        //Create
+        //        return View(new Company());
+        //    }
+        //    else
+        //    {
+        //        //Edit
+        //        Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+        //        return View(companyObj);
+        //    }
+
+        //}
+
+        //// GET: CompanyController/Details/5
+        //public IActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: CompanyController/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: CompanyController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(Company obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Company.Add(obj);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Company created successfully";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(obj);
+        //}
+
+        // GET: CompanyController/Edit/5
+        /*public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Company? companyFromDb = _unitOfWork.Company.Get(u => u.Id == id);
+
+            if(companyFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(companyFromDb);
+        }*/
+
+        // POST: CompanyController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit(Company obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Company.Update(obj);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Company edited successfully";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(obj);
+        //}
+
+        // GET: CompanyController/Delete/5
+        /*public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Company companyFromDb = _unitOfWork.Company.Get(u => u.Id == id);
+
+            if(companyFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(companyFromDb);
+        }*/
+
+        // POST: CompanyController/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult DeleteCompany(int? id)
+        //{
+        //    Company obj = _unitOfWork.Company.Get(u => u.Id == id);
+        //    if(obj == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    _unitOfWork.Company.Remove(obj);
+        //    _unitOfWork.Save();
+        //    TempData["success"] = "Company deleted successfully";
+        //    return RedirectToAction("Index");
+        //}
+
+        public IActionResult RoleManagement(string userId)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+            RoleManagementVM RoleVM = new RoleManagementVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            return View(RoleVM);
+        }
+
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            if (!(roleManagementVM.ApplicationUser.Role == oldRole))
+            {
+                //A role was updated
+                ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+                if (roleManagementVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in objUserList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company
+                    {
+                        Name = ""
+                    };
+                }
+            }
+            return Json(new { data = objUserList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error whole locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked and we need to unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                //lock the user
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation successful" });
+        }
+        #endregion
+    }
+}
